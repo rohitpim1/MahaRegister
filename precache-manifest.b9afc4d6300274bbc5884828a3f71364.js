fd058ebc59711a21bfd5872c4cd523c0b0362294
rohitpim1 <pimrohit33@gmail.com>
2020-10-09 18:20:14 +0530

    Updates

---
+++ precache-manifest.b9afc4d6300274bbc5884828a3f71364.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85c62120552f0679c4a3e56027b41421",
+    "url": "/MahaRegister/index.html"
+  },
+  {
+    "revision": "c32de3f855f3deaa8182",
+    "url": "/MahaRegister/static/css/2.ea6b7726.chunk.css"
+  },
+  {
+    "revision": "a54b5e936dd1234a5633",
+    "url": "/MahaRegister/static/css/main.9d8675a8.chunk.css"
+  },
+  {
+    "revision": "c32de3f855f3deaa8182",
+    "url": "/MahaRegister/static/js/2.db32e30d.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/MahaRegister/static/js/2.db32e30d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a54b5e936dd1234a5633",
+    "url": "/MahaRegister/static/js/main.c30d050d.chunk.js"
+  },
+  {
+    "revision": "2c74e74acd54b5fe6b84",
+    "url": "/MahaRegister/static/js/runtime-main.f188920b.js"
+  },
+  {
+    "revision": "91157a46bcefbbad39176655b610f966",
+    "url": "/MahaRegister/static/media/bg.91157a46.png"
+  },
+  {
+    "revision": "19f371d60f5fa749fd6ad2fd3e6d5e11",
+    "url": "/MahaRegister/static/media/msme.19f371d6.jpg"
+  }
+]);
